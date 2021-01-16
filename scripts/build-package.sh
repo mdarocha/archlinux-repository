fcb50fd20c2a114ffbd2e75a6d392f292fd2204b
mdarocha <git@mdarocha.pl>
2021-01-16 21:27:42 +0100

    set PKGDEST correctly

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -9,10 +9,18 @@ CONTEXT=${GITHUB_WORKSPACE:=.}
 PKGREPO="${CONTEXT}/package"
 PKGDEST="${CONTEXT}/build"
 
+mkdir -p ${PKGREPO}
+mkdir -p ${PKGDEST}
 
+# clone pkgfile from aur
 git clone https://aur.archlinux.org/${PACKAGE}.git ${PKGREPO}
 
+# setup PKGDEST
+sudo sh -c "echo 'PKGDEST=${PKGDEST}' >> /etc/makepkg.conf"
+
+# enter cloned repo and run makepkg
 cd ${PKGREPO}
 makepkg -c -s --noconfirm
 
+# go back to context
 cd ${CONTEXT}
